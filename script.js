d1f4af85d33e6f4ce5f8a79d2946340da7991162
chizz <cbrittingham@gmail.com>
2020-02-20 12:21:25 -0600

    added code to accept user input for password length

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// Accept password length input
 var passwordLength = parseInt(prompt("How long would you like the password to be? (between 8 and 128 characters)"));
+
+//Verify password length is within bounds (8-128)
+if (passwordLength < 8 || passwordLength > 128){
+    console.log("password invalid. length: " + passwordLength);
+    var validLength = false;
+    do{
+        console.log("length before new prompt: "+ passwordLength);
+        passwordLength = parseInt(prompt("Enter a number between 8 and 128"));
+        console.log("legnth after new input: "+ passwordLength);
+  
+        console.log("current bool state in while loop: "+ validLength);
+        if (passwordLength > 8 && passwordLength < 128){
+          validLength = true;
+          console.log("current bool state in if statement: " + validLength);
+        }
+      } while (validLength === false)
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
 
 
 // Write password to the #password input
