ba80d550407f724151435929eda31df1aef407b7
chizz <cbrittingham@gmail.com>
2020-02-20 17:27:10 -0600

    committing a bit late, added arrays for alphabets numbers and special characters and a test for checkbox functionality

--- script.js
+++ script.js
@@ -15,12 +15,74 @@ if (passwordLength < 8 || passwordLength > 128){
         console.log("legnth after new input: "+ passwordLength);
   
         console.log("current bool state in while loop: "+ validLength);
-        if (passwordLength > 8 && passwordLength < 128){
+        if (passwordLength > 8 && passwordLength < 129){
           validLength = true;
           console.log("current bool state in if statement: " + validLength);
         }
       } while (validLength === false)
   }
+   
+
+  var checkBoxArray = ["lowercase", "uppercase", "numeric", "special"];
+
+//   Generate lowercase arrray and number array from strings
+  var alph = "abcdefghijklmnopqrstuvwxyz";
+  var num = "0987654321";
+  var spec ="!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  var specialArray = [];
+  var lowercaseArray = [];
+  var numberArray = [];
+  lowercaseArray = alph.split("");
+  numberArray = num.split("");
+  specialArray = spec.split("");
+
+  console.log(numberArray);
+  console.log(specialArray);
+
+
+  console.log(lowercaseArray);
+  // generate uppercase array from lowercase array
+  var uppercaseArray = makeUpper(lowercaseArray); 
+
+  console.log(uppercaseArray);
+
+  function makeUpper(a){
+      var u = [];
+      for(i=0; i<a.length; i++){
+        u.push(a[i].toUpperCase());
+      }
+      return u
+  }
+
+
+  var myDiv = document.getElementById("checkboxes"); 
+              
+            // creating checkbox element 
+            var checkbox = document.createElement('input'); 
+
+            console.log(checkbox);
+              
+            // Assigning the attributes to created checkbox
+            checkbox.type = "checkbox"; 
+            checkbox.name = "name"; 
+            checkbox.value = "value"; 
+            checkbox.id = "id"; 
+
+            console.log(myDiv);
+              
+            // creating label for checkbox 
+            var label = document.createElement('label'); 
+              
+            // assigning attributes for the created label tag   
+            label.htmlFor = "id"; 
+              
+            // appending the created text to  the created label tag  
+            label.appendChild(document.createTextNode("Textbox label here"));
+              
+            // appending the checkbox 
+            // and label to div 
+            myDiv.appendChild(checkbox); 
+            myDiv.appendChild(label); 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -32,6 +94,11 @@ generateBtn.addEventListener("click", writePassword);
 function writePassword() {
     // var password = generatePassword();
     var passwordText = document.querySelector("#password");
+
+    var box = document.getElementById("id");
+    console.log(box);
+    console.log(box.checked);
+    
   
     
     passwordText.value = password;
